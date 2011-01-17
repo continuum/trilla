d39f4fccf691030110c3c8045f28112ceefa1474
Leo Soto M <leo.soto@gmail.com>
2011-01-17 12:46:52 -0300

    Borrando clase TareaOrder no usada en ninguna parte

--- app/models/tarea.rb
+++ app/models/tarea.rb
@@ -3,10 +3,3 @@ class Tarea < ActiveRecord::Base
   named_scope :no_facturables, :conditions => "tipo = 'No Facturable'"
   named_scope :ordered_by_tipo, :order => "tipo"
 end
-
-class TareaOrder
-  
-  attr_accessor :descripcion
-  
-  
-end
