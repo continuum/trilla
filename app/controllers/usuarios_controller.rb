5fcda536cd7016252c5e48efa4f7b1bd7f5bb491
Leo Soto M <leo.soto@gmail.com>
2011-01-17 15:44:58 -0300

    Simplificado controlador de usuarios

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,109 +1,57 @@
 class UsuariosController < AdminController
-  # GET /usuarios
-  # GET /usuarios.xml
   def index
     @usuarios = Usuario.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @usuarios }
-    end
   end
 
-  # GET /usuarios/1
-  # GET /usuarios/1.xml
   def show
-    
     @usuario = Usuario.find(params[:id])
     @usuario_sesion = session[:usuario]
     @isPerfil = params[:p]
-
     if (!@isPerfil.nil?)
-    
       if (@usuario.id != @usuario_sesion.id)
         @usuario = @usuario_sesion
       end
-      
     end
-    
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @usuario }
-    end
-    
   end
 
-  # GET /usuarios/new
-  # GET /usuarios/new.xml
   def new
     @usuario = Usuario.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @usuario }
-    end
   end
 
-  # GET /usuarios/1/edit
   def edit
-    
     @usuario = Usuario.find(params[:id])
     @usuario_sesion = session[:usuario]
     @isPerfil = params[:p]
-    
     if (!@isPerfil.nil?)
-    
       if (@usuario.id != @usuario_sesion.id)
         @usuario = @usuario_sesion
       end
-
     end
-    
   end
 
-  # POST /usuarios
-  # POST /usuarios.xml
   def create
     @usuario = Usuario.new(params[:usuario])
-
-    respond_to do |format|
-      if @usuario.save
-        flash[:notice] = 'Usuario was successfully created.'
-        format.html { redirect_to(@usuario) }
-        format.xml  { render :xml => @usuario, :status => :created, :location => @usuario }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @usuario.errors, :status => :unprocessable_entity }
-      end
+    if @usuario.save
+      flash[:notice] = 'Usuario was successfully created.'
+      redirect_to(@usuario)
+    else
+      render :action => "new"
     end
   end
 
-  # PUT /usuarios/1
-  # PUT /usuarios/1.xml
   def update
     @usuario = Usuario.find(params[:id])
-
-    respond_to do |format|
-      if @usuario.update_attributes(params[:usuario])
-        flash[:notice] = 'Usuario was successfully updated.'
-        format.html { redirect_to(@usuario) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @usuario.errors, :status => :unprocessable_entity }
-      end
+    if @usuario.update_attributes(params[:usuario])
+      flash[:notice] = 'Usuario was successfully updated.'
+      redirect_to(@usuario)
+    else
+      render :action => "edit"
     end
   end
 
-  # DELETE /usuarios/1
-  # DELETE /usuarios/1.xml
   def destroy
     @usuario = Usuario.find(params[:id])
     @usuario.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(usuarios_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(usuarios_url)
   end
 end
