890150499a3c73c5825d05354ae8827a616493a5
Leo Soto M <leo.soto@gmail.com>
2011-03-09 15:47:05 -0300

    login helper corregido

--- spec/acceptance/support/helpers.rb
+++ spec/acceptance/support/helpers.rb
@@ -1,10 +1,14 @@
 module HelperMethods
   def login
     visit "/"
-    fill_in 'Email', :with => USERNAME
-    fill_in 'Passwd', :with => PASSWORD
-    click_button "Sign in"
-    click_button "Conceder acceso"
+    # By default google stores the session in our browser, so this might not
+    # be needed
+    if page.has_content? "requesting permission"
+      fill_in 'Email', :with => USERNAME
+      fill_in 'Passwd', :with => PASSWORD
+      click_button "Sign in"
+      click_button "Conceder acceso"
+    end
   end
 end
 
