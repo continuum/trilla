72e18c825f2ae9cf05a0cb0a68bc6da86e206eef
Alter Lagos <alter.lagos@continuum.cl>
2011-03-10 16:39:53 -0300

    Prueba de login arrojaba error si cuenta de google estaba configurada con otro idioma. Ahora se verifica existencia de elementos HTML

--- spec/acceptance/login_spec.rb
+++ spec/acceptance/login_spec.rb
@@ -6,16 +6,16 @@ feature "Login" do
   end
 
   scenario "System shows a google account login", :js => true do
-    page.should have_content("A third party service is requesting permission to access")
-    page.should have_content("Continuum Ltda account")
+    page.should have_xpath("//form[@action='https://www.google.com/a/continuum.cl/LoginAction2?service=apps']")
   end
 
   scenario "Logs in with the right credentials", :js => true do
     fill_in 'Email', :with => USERNAME
     fill_in 'Passwd', :with => PASSWORD
-    click_button "Sign in"
-    page.should have_content("ha solicitado acceso a tu Cuenta de Continuum")
-    click_button "Conceder acceso"
+    click_button "signIn"
+    page.should have_xpath("//input[@id='allow']")
+    page.should have_xpath("//input[@id='deny']")
+    click_button "allow"
     page.should have_content("Trilla Time Tracking")
   end
 end
