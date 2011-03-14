061aa0ff02c4756e79630db2f694072c654c06f8
Leo Soto M <leo.soto@gmail.com>
2011-03-14 14:43:53 -0300

    Mas correcciones en specs

--- spec/acceptance/support/helpers.rb
+++ spec/acceptance/support/helpers.rb
@@ -3,12 +3,19 @@ module HelperMethods
     visit "/"
     # By default google stores the session in our browser, so this might not
     # be needed
-    if page.has_content? "requesting permission"
+    if page.has_xpath? "//input[@id='signIn']"
       fill_in 'Email', :with => USERNAME
       fill_in 'Passwd', :with => PASSWORD
-      click_button "Sign in"
-      click_button "Conceder acceso"
+      click_button "signIn"
+      click_button "allow"
     end
+  end
+
+  def logout
+    visit "/"
+    click_link "Salir" if page.has_link? "Salir" # logs out of trilla
+    click_link "Logout" if page.has_link? "Logout" # logs out of google, english
+    click_link "Salir" if page.has_link? "Salir" # logs out of google, spanish
   end
 end
 
