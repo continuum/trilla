be45516fdf8c98dfdcb280a60f38972b6756a773
Alter Lagos <alter.strife@gmail.com>
2011-03-24 09:50:07 -0300

    Validaciones de campo mejoradas

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -6,24 +6,15 @@ class Usuario < ActiveRecord::Base
   validates_presence_of :email, :message => "Debe ingresar el e-mail del usuario"
   validates_presence_of :perfil, :message => "Debe ingresar un perfil"
   #TODO Cuando se definan estándar de perfiles, cambiar esta validación
-  validates_inclusion_of :perfil, :in => %w( ADMIN USUARIO ), :if => :perfil_con_datos?, :message => "Perfil inválido. Valores válidos son: ADMIN, USUARIO"
-  validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :if => :email_con_datos?, :message => 'Formato de e-mail inválido'
+  validates_inclusion_of :perfil, :in => %w( ADMIN USUARIO ), :if => :perfil?, :message => "Perfil inválido. Valores válidos son: ADMIN, USUARIO"
+  validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :if => :email?, :message => 'Formato de e-mail inválido'
   validates_uniqueness_of :email, :message => "Ya existe un usuario con esta dirección de e-mail"
   
   def self.find_by_login(login)
     find(:first, :conditions => ["rut = ?",login])
   end
-  
+
   def self.autenticar(login, password)
     find(:first, :conditions => ["rut = ? and password = ?",login, password])
-  end
-  
-  def perfil_con_datos?
-    !perfil.lstrip.empty?
-  end
-  
-  def email_con_datos?
-    !email.lstrip.empty?
-  end
-  
+  end  
 end
