4f599fbe60f02c1f8f1dbe07f94a8b8f50f9099f
Leo Soto M <leo.soto@gmail.com>
2011-05-25 12:42:36 -0400

    Correccion a error de sintaxis originada por un enredo

--- app/models/proyecto.rb
+++ app/models/proyecto.rb
@@ -1,10 +1,10 @@
-1class Proyecto < ActiveRecord::Base
+class Proyecto < ActiveRecord::Base
   belongs_to :cliente
   has_many :proyecto_tareas
   has_many :tareas, :through => :proyecto_tareas
   accepts_nested_attributes_for :proyecto_tareas, :reject_if => lambda { |a| a.values.all?(&:blank?) }, :allow_destroy => true
 
-  
+
   has_many :proyecto_usuarios
   has_many :usuarios, :through => :proyecto_usuarios
   accepts_nested_attributes_for :proyecto_usuarios, :reject_if => lambda { |a| a.values.all?(&:blank?) }, :allow_destroy => true
@@ -17,18 +17,18 @@
   #validates_associated :cliente
 
   named_scope :no_archivados, :conditions => "not archivado"
-  
+
   def validate
     if self.privado == true && self.proyecto_usuarios.size <= 0 && self.usuarios.size <= 0
       errors.add_to_base "Debe asociar al menos un usuario a un proyecto privado"
       false
     end
   end
-  
+
   def self.find_all_accesibles(usuario_id)
    # find(
-   #   :all, 
-   #   :joins => "left join proyecto_usuarios on proyecto_usuarios.proyecto_id = proyectos.id", 
+   #   :all,
+   #   :joins => "left join proyecto_usuarios on proyecto_usuarios.proyecto_id = proyectos.id",
    #   :conditions => ["privado = false or privado is null or proyecto_usuarios.usuario_id = ?", usuario_id]
    # )
    #TODO:  BUG N°: 13273349
@@ -46,13 +46,13 @@
                                FROM proyectos left join proyecto_usuarios on proyecto_usuarios.proyecto_id = proyectos.id
                                WHERE privado = false or privado is null or proyecto_usuarios.usuario_id = #{usuario_id}")
   end
-  
+
   def archive
     self.update_attributes({:archivado => true})
-  end 
+  end
 
   def restore
-    self.update_attributes({:archivado => false})    
+    self.update_attributes({:archivado => false})
   end
 
 end
