238b590cb2fc9de26144efa3eafb7b6a14277e03
Eduardo Offermann <eofferma@gmail.com>
2011-06-18 16:58:55 -0400

    Se corrige la obtencion de la fecha del ultimo commit debido a que si ocurre un merge la fecha se encontrara en la cuarta linea de la salida del comando git log

--- config/initializers/git_info.rb
+++ config/initializers/git_info.rb
@@ -1,3 +1,5 @@
 GIT_BRANCH = `git status | sed -n 1p`.split(" ").last
 GIT_COMMIT = `git log | sed -n 1p`.split(" ").last
-GIT_LAST_COMMIT_DATE = `git log | sed -n 3p`.split(" ").join(" ")
+date_or_author = `git log | sed -n 3p`
+# If a merge occurs the date of the commit will be on the fourth line.
+GIT_LAST_COMMIT_DATE = date_or_author.include?('Date')? date_or_author : `git log | sed -n 4p`
